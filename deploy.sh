6eea733a44e5859bd560fd384bb8ee4a8f6adb87
Rocky Chen <Rocky.Chen@dbca.wa.gov.au>
2024-05-20 14:39:12 +0800

    Fix some isssues

--- deploy.sh
+++ deploy.sh
@@ -17,10 +17,10 @@ mkdir /tmp/_helm_deploy
 cp -rf ${helm_dir}/* /tmp/_helm_deploy
 
 #remove all comments lines
-sed -i -E '/^\s*#.*/d' /tmp/_helm_deploy/templates/*
+sed -i -E '/^\s*#[^!]*/d' /tmp/_helm_deploy/templates/*
 
 #remove all comments in a line
-sed -i -E 's/#.*$//g' /tmp/_helm_deploy/templates/*
+sed -i -E 's/#[^!]*$//g' /tmp/_helm_deploy/templates/*
 
 echo "helm $@ /tmp/_helm_deploy"
 helm $@ /tmp/_helm_deploy
