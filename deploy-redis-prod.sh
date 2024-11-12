d4e54142b4498c496aab445d1543daf1436ba1da
Rocky Chen <Rocky.Chen@dbca.wa.gov.au>
2024-11-12 09:20:12 +0800

    Change the var name in script

--- deploy-redis-prod.sh
+++ deploy-redis-prod.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-isuat=$(kubectl config get-contexts | grep  -E "\*\s+az-aks-prod01" | wc -l)
-if [[ ${isuat} -eq 1 ]]; then
+isprod=$(kubectl config get-contexts | grep  -E "\*\s+az-aks-prod01" | wc -l)
+if [[ ${isprod} -eq 1 ]]; then
     echo "Kubectl is connected to az-aks-prod01, begin to deploy redis in prod env"
 else
     echo "Kubectl is not connected to az-aks-prod01, can't deploy redis in prod env"
