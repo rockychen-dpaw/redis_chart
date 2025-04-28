880e0edfb8a6e775141aac6c4ed1662050c1f0b4
Rocky Chen <Rocky.Chen@dbca.wa.gov.au>
2025-04-28 10:14:30 +0800

    some changes in values yaml file

--- deploy-redis-coe-prod-02.sh
+++ deploy-redis-coe-prod-02.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-isprod=$(kubectl config get-contexts | grep  -E "\*\s+az-coe-prod-02" | wc -l)
+isprod=$(kubectl config get-contexts | grep  -E "\*\s+aks-coe-prod-02" | wc -l)
 if [[ ${isprod} -eq 1 ]]; then
-    echo "Kubectl is connected to az-coe-prod-02, begin to deploy redis in prod env"
+    echo "Kubectl is connected to aks-coe-prod-02, begin to deploy redis in prod env"
 else
-    echo "Kubectl is not connected to az-coe-prod-02, can't deploy redis in prod env"
+    echo "Kubectl is not connected to aks-coe-prod-02, can't deploy redis in prod env"
     exit 1
 fi
 
